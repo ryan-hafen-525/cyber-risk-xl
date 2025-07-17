eaaa59bcd93c20999dbcc51885e8d58b1bb2dbf2
Ryan Hafen <rh525@byu.edu>
2025-07-16 20:42:03 -0600

    added landing page

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,5 +1,16 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function LandingPage() {
-
+    return (
+        <div>
+            <h1>Cyber Risk XL</h1>
+            <p>A lightweight excel-powered tool for assessing cyber risk.</p>
+            <div className="row">
+                <button type="submit">Create new risk register</button>
+                <button type="submit">Open existing risk register</button>
+            </div>
+        </div>
+    )
 }
+
+export default LandingPage;
